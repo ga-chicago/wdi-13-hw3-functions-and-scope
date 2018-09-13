11cd5f486123c7c015251544e77f56864c5621fe
Chris Roman <Christopherroman85@gmail.com>
2018-09-13 18:59:29 -0500

    Commit 1 - Verbal questions

---
+++ js/app.js
@@ -0,0 +1,14 @@
+// console.log('it is working')
+
+// Verbal Questions
+
+// 1. A parameter is a variable within a function definition. An arguement
+// is the data you pass through a function
+
+// 2. Within a function return will spit out the value that has been
+//    stored within a variable. Console.log will just print the info
+//	  into the console and no data is stored
+
+// 3. The value that is stored within a function can then be called
+//	  later on in your code.
+
