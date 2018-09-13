a585328031ae472f8dd0211e3420f7caf8a4d32d
Josh Hepworth <jrhepworth12@gmail.com>
2018-09-13 18:21:40 -0500

    Commit-1 Verbal questions

---
+++ js/app.js
@@ -0,0 +1,15 @@
+console.log('hello');
+
+//VERBAL QUESTIONS
+
+//1. What is the difference between a parameter and an argument?
+
+// The parameters are within the function definition, and the arguments are the values that you pass into the function that you call.
+
+// 2. Within a function, what is the difference between return and console.log?
+
+//Returning a value produces it onto the page, console log is for the user to check the work(verifying it works).
+
+//3. What are the implications of the ability of a function to return a value?
+
+//By returning the value, it actually gives you a tangeable function that you can use.
