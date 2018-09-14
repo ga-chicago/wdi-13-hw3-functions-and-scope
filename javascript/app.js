69f49c89cd1126e8be6b29198af1335712f08d81
Andy Prudhomme <aprudhomme9@gmail.com>
2018-09-14 07:51:52 -0500

    Commit 1 - verbal questions

---
+++ javascript/app.js
@@ -0,0 +1,7 @@
+console.log("up and running");
+
+
+// 1. A parameter is a varible that is used to declare a function within the (), while an argument consists of the actually values you pass into a function when you invoke that function i.e. const someFunction (parameter) => {} vs. someFunction(argument);
+// 2. Return gives the function a value within your program when the program is run while console.log will display the result of the invoked function within your console
+// 3. The ability to return a value within a function allows you to store this value within a variable or call upon the result after it is returned
+
