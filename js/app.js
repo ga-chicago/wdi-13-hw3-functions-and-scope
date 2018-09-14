d34ce38cb79b6334118ce0e1859fea154139429e
Josh Hepworth <jrhepworth12@gmail.com>
2018-09-13 19:51:24 -0500

    Commit 2-Palindrome

--- js/app.js
+++ js/app.js
@@ -13,3 +13,26 @@ console.log('hello');
 //3. What are the implications of the ability of a function to return a value?
 
 //By returning the value, it actually gives you a tangeable function that you can use.
+
+
+
+// 2. PALINDROME AGAIN
+
+// Write a function checkPalindrome that accepts a single argument, a string. Yes, you've done it before, but do it again. Later in this assignment we're gonna beef up our palindrome function some. See if you can do it without looking back at your previous answer. The function should return true if the string is a palindrome, false if not. Make sure your function will give the correct answer for words with capital letters.
+
+
+
+const checkPalindrome = (words) => {
+	const reverseWord = words.split('').reverse().join('');
+	// console.log(reverseWord);
+	console.log(words.toUpperCase());
+if(words.toUpperCase() === reverseWord.toUpperCase()) {
+	return true;
+} else {
+	return false;
+}
+}
+
+ console.log(checkPalindrome('Mom'));
+
+
