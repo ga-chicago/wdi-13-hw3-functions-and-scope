82b1b7027cab248f400c5ad2ef17ae249fd3be3d
Gregory Frye <Swayze@Gregorys-MacBook-Pro.local>
2018-09-15 16:31:44 -0500

    Commit 1 - Verbal Questions

---
+++ js/app.js
@@ -0,0 +1,17 @@
+// 1. Verbal questions
+
+// a parameter is a variable that lives inside of a method
+// an argument is the value of after its been passed through the method
+// is it possible then to have your parameter and agrument be the same?
+// thats what it looks like in my Palindrome?
+
+// return is giving you the value of the function 
+// where console.log is just showing the output in the console
+
+// Other than returing a function value you can reuse it, pass it through an argument?
+
+
+
+
+
+
