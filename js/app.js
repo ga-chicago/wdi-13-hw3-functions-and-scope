93e809dde7400b4d995c5e67f8521723c2940225
Gregory Frye <Swayze@Gregorys-MacBook-Pro.local>
2018-09-15 23:02:56 -0500

    Commit 6 - Prime Numbers.

--- js/app.js
+++ js/app.js
@@ -12,41 +12,38 @@
 
 
 // 2. Palindrome
-// const checkPalindrome = (word) => {
-// 	// console.log(word.split("").reverse().join(""))
-// 	word = word.toLowerCase()
-// 	const reversedWord = word.toLowerCase().split("").reverse().join("")
-// 	return reversedWord === word;
-// 	}
+const checkPalindrome = (word) => {
+	console.log(word.split("").reverse().join(""))
+	word = word.toLowerCase()
+	const reversedWord = word.toLowerCase().split("").reverse().join("")
+	return reversedWord === word;
+	}
 	
 
-// console.log(checkPalindrome("Borscht"));
+console.log(checkPalindrome("Borscht"));
 
-// console.log(checkPalindrome("Radar"));
+console.log(checkPalindrome("Radar"));
    
 
 // 3.Sum of Digits
 
 
-// const sumDigits = (num) => {
-//     return (num + 4 + 2) % 10 - 2 ;
-// // I know I got this but I'm a litle confused still
-// // return (num + 4 + 2) % 10 gets me with the remainder of 6 but why -2?
-// // when I tried it without I would get 8, just a little foggy on this
-// }
+const sumDigits = (num) => {
+    return (num + 4 + 2) % 10 - 2 ;
+// I know I got this but I'm a litle confused still
+// return (num + 4 + 2) % 10 gets me with the remainder of 6 but why -2?
+// when I tried it without I would get 8, just a little foggy on this
+}
 
-// console.log(sumDigits(42));
+console.log(sumDigits(42));
 
-// Write a function calculateSide that takes two arguments: sideA and sideB, and returns the solution for sideC using the Pythagorean theorem.
-// hint: discover the Pythagorean Theorem on a website called google.com
-// hint: checkout the Math methods in javascript
-//declared variable 
+// Pythagoras
 
-// const calculateSide = (sideA, sideB) => {
+const calculateSide = (sideA, sideB) => {
     
-//     return Math.sqrt(Math.pow(sideA, 2) + Math.pow(sideB, 2));
+    return Math.sqrt(Math.pow(sideA, 2) + Math.pow(sideB, 2));
 
-// }
+}
 
 // Sum Array
 
@@ -56,6 +53,27 @@ const sum = [1, 2, 3, 4, 5, 6].reduce(function (accumulator, currentValue) {
 }, 0);
 
 console.log(sum);
+
+
+
+// Prime Numbers
+		
+function checkPrime(num) {
+    for(let i = 2; i < num; i++) {
+        if(num % i === 0) {
+            return false;
+        }
+    }
+    return num > 1;
+}
+console.log(checkPrime(2));
+
+// I'm not sure where to go from here
+// I did some googling and found some 
+// code that could probaly work, 
+// but I didnt understand how 
+// and didnt just want to copy and paste
+// to have an answer
 
 
 
